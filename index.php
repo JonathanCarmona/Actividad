cf3e2b42d94a5efe3d783985183a433a77f9b2ec
JonathanCarmona <carmona.jonathan@icloud.com>
2017-03-01 00:13:48 -0600

    Add files via upload

--- index.php
+++ index.php
@@ -12,6 +12,16 @@ $app->get('/clima', function() use($app) {
     return $app->json($arreglo);
 });
 
+$client = new GuzzleHttp\Client([
+'base_url' => 'http://openweathermap.org/current',
+'timeout' => 2.0,
+]);
+$id = 3515062;
+$res = $client->request('GET', 'http://api.openweathermap.org/data/2.5/weather?id='. $id .'&APPID=258a11d1c99a32c8451688e82b7c893e', [
+
+   
+]);
+return $res->getBody();
 
 $app->run();
 
